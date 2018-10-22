76ce9e836c61f5ca7b9b986abce1cbda5754d654
Yoel Horvitz <yoelhor@live.com>
2018-10-22 08:40:49 +0300

    Adding JAVA deployment

--- policies/rest-api-integration/Source-Code/Java-Spring/src/main/java/com/aadb2c/webapi/B2CResponseModel.java
+++ policies/rest-api-integration/Source-Code/Java-Spring/src/main/java/com/aadb2c/webapi/B2CResponseModel.java
@@ -4,8 +4,6 @@ public class B2CResponseModel {
     private int status;
     private String version;
     private String userMessage;
-    private String loyaltyNumber;
-    private String email;
 
     public B2CResponseModel() {
         this.setVersion("1.0.0");
@@ -36,26 +34,6 @@ public class B2CResponseModel {
 
     public void setUserMessage(String userMessage) {
         this.userMessage = userMessage;
-    }
-
-    
-    // User message
-    public String getLoyaltyNumber() {
-        return loyaltyNumber;
-    }
-
-    public void setLoyaltyNumber(String loyaltyNumber) {
-        this.loyaltyNumber = loyaltyNumber;
-    }
-
-    
-    // User message
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
 
